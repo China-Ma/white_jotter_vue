97a3e1e58d219d839a3ee5496d4300e8b7488099
guoce <majiaju96@163.com>
2020-11-16 17:18:54 +0800

    注册、登录加密功能

--- src/router/index.js
+++ src/router/index.js
@@ -4,6 +4,7 @@ import AppIndex from '@/components/home/AppIndex'
 import Login from '@/components/Login'
 import Home from '@/components/Home'
 import LibraryIndex from '../components/library/LibraryIndex'
+import Register from '../components/Register'
 Vue.use(Router)
 
 export default new Router({
@@ -47,6 +48,11 @@ export default new Router({
       path: '/login',
       name: 'Login',
       component: Login
+    },
+    {
+      path: '/register',
+      name: 'Register',
+      component: Register
     }
   ]
 })
